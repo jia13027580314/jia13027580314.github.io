4612792226457f4dd66a3e569e3ac7eaf953d355
jiazhipeng <824504808@qq.com>
2019-12-24 18:46:44 +0800

    aa

---
+++ precache-manifest.cf2b4ba425e48581753741e5766e80a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f602bef6e1de76250bad986e8b102d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d6f6bff5ef4eb8ccae",
+    "url": "/static/css/main.053feb23.chunk.css"
+  },
+  {
+    "revision": "deb1c279fd8e54d72672",
+    "url": "/static/js/2.7928707c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.7928707c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6d6f6bff5ef4eb8ccae",
+    "url": "/static/js/main.e3b4a308.chunk.js"
+  },
+  {
+    "revision": "55c0ae6422a0c960795d",
+    "url": "/static/js/runtime-main.79ca8e13.js"
+  }
+]);
